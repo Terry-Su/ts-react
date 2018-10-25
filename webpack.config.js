c28dc4687be42a3a2cd1782b6fee985aa1e5050b
TerrySu <2684759855@qq.com>
2018-10-25 17:47:44 +0800

    feat(init)

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require( 'path' );
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require( 'copy-webpack-plugin' )
+
+
+module.exports = {
+	mode: "development",
+	entry: {
+		'tsreact.js': './src/index.ts',
+	},
+	output: {
+		filename: '[name]',
+		path: path.resolve( __dirname, 'build' )
+	},
+	devtool: 'source-map',
+	module: {
+		rules: [
+			{
+				test: /\.ts?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/
+			}
+		]
+	},
+	resolve: {
+		alias: {
+		},
+		extensions: [
+			'.ts',
+			'.js'
+		],
+	},
+	plugins: [
+		new CleanWebpackPlugin( [ 'build' ] ),
+		// new CopyWebpackPlugin( [
+		// ] ),
+	]
+};
